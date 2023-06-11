02543b0533271f26aa9b99fafad1394097a0b4fa
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2023-06-11 18:15:03 +0300

    Initial Commit

---
+++ baheth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  baheth
+//
+//  Created by علي فاضل on 11/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
